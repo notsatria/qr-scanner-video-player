0dc7f4f16dacf620acb9e828d29e6d734bbf28f0
notsatria <buanasatriaa@gmail.com>
2025-02-16 21:52:40 +0700

    feat: add barcode view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_video_player/app/app.dart';
+import 'package:qr_video_player/app/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ChewieDemo(),
+      home: HomePage(),
     );
   }
 }
