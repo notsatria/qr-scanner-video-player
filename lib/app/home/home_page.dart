cc883f073e209a4fc85bead12fdb9e76aa56696f
notsatria <buanasatriaa@gmail.com>
2025-02-19 21:13:40 +0700

    feat: update home page

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:qr_video_player/app/model/video_result.dart';
 import 'package:qr_video_player/app/scanner/scanner_page.dart';
 import 'package:qr_video_player/helper/database_helper.dart';
@@ -22,7 +23,17 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Ruang Ngaji Kita',
+          style: TextStyle(color: theme.colorScheme.onPrimary),
+        ),
+        centerTitle: true,
+        backgroundColor: theme.primaryColor,
+      ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(16.0),
         child: FloatingActionButton.extended(
@@ -35,13 +46,153 @@ class _HomePageState extends State<HomePage> {
               children: [Icon(Icons.qr_code_scanner), Text("Scan QR")],
             )),
       ),
-      body: ListView.builder(
-        itemCount: videoResult.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(videoResult[index].title),
-          contentPadding: const EdgeInsets.only().copyWith(left: 12, right: 8),
-          trailing: IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0)
+                .copyWith(top: 20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Cari Video',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                hintText: 'Cari Video',
+                suffixIcon: const Icon(Icons.search),
+              ),
+              onChanged: (query) async {
+                final result = await dbHelper.searchVideos(query);
+                setState(() {
+                  videoResult = result;
+                });
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          (videoResult.isNotEmpty)
+              ? Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: videoResult.length,
+                    itemBuilder: (context, index) =>
+                        _slidableItem(index, theme),
+                  ),
+                )
+              : _emptyStateWidget()
+        ],
+      ),
+    );
+  }
+
+  Slidable _slidableItem(int index, ThemeData theme) {
+    return Slidable(
+      key: ValueKey(index),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: theme.colorScheme.primary,
+            foregroundColor: Colors.white,
+            icon: Icons.edit,
+            label: 'Edit',
+          ),
+          SlidableAction(
+            onPressed: (context) {
+              showDeleteConfirmDialog(
+                  videoResult[index].id!, videoResult[index].title);
+            },
+            backgroundColor: theme.colorScheme.error,
+            foregroundColor: Colors.white,
+            icon: Icons.restore_from_trash,
+            label: 'Hapus',
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: Container(
+          height: 35,
+          width: 35,
+          decoration: ShapeDecoration(
+            shape: StarBorder(
+              side: BorderSide(color: theme.primaryColor),
+              points: 10.00,
+              rotation: 0.00,
+              innerRadiusRatio: 0.60,
+              pointRounding: 0.35,
+              valleyRounding: 0.00,
+              squash: 0.00,
+            ),
+          ),
+          child: Center(
+              child: Text(
+            "${index + 1}".toString(),
+            style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: theme.colorScheme.onPrimaryContainer),
+          )),
         ),
+        title: Text(
+          videoResult[index].title,
+          style: theme.textTheme.titleMedium?.copyWith(
+            fontSize: 18,
+            color: theme.colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: (videoResult[index].description != null)
+            ? Text(
+                videoResult[index].description!,
+                style: theme.textTheme.labelLarge
+                    ?.copyWith(color: theme.colorScheme.outline),
+              )
+            : null,
+        contentPadding: const EdgeInsets.only().copyWith(left: 12, right: 8),
+      ),
+    );
+  }
+
+  void showDeleteConfirmDialog(int id, String title) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Konfirmasi'),
+              content: Text('Apakah Anda yakin ingin menghapus item $title?'),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Batal')),
+                TextButton(
+                    onPressed: () {
+                      dbHelper.delete(id);
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Hapus'))
+              ],
+            ));
+  }
+
+  Widget _emptyStateWidget() {
+    return Container(
+      width: 500,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/quran.png',
+            height: 220,
+          ),
+          const Text(
+            'Anda belum mengaji hari ini. Klik Scan QR untuk memulai.',
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qr_video_player/app/home/home_page.dart';
+import 'package:qr_video_player/utils/util.dart';
+import 'package:qr_video_player/utils/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,12 +14,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    TextTheme textTheme = createTextTheme(context, "Source Sans 3", "Source Sans 3");
+    MaterialTheme theme = MaterialTheme(textTheme);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Ruang Ngaji Kita',
+      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
       home: const HomePage(),
     );
   }
